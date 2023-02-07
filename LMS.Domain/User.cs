aeb149eda4a9f1b66d62c24c03f8a458e38c51fd
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-02-07 22:48:48 +0500

    User domain model and UserRole enum added

---
+++ LMS.Domain/User.cs
@@ -0,0 +1,15 @@
+﻿using LMS.Domain.Enums;
+
+namespace LMS.Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public UserRole Role { get; set; }
+        public decimal Gpa { get; set; }
+
+
+    }
+}
