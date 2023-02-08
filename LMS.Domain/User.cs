f4318c24326d7c1eb920df3ea24e18027cf7b2ef
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-02-08 12:15:24 +0500

    user password field added

--- LMS.Domain/User.cs
+++ LMS.Domain/User.cs
@@ -7,6 +7,7 @@ namespace LMS.Domain
         public int Id { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
         public UserRole Role { get; set; }
         public decimal Gpa { get; set; }
 
