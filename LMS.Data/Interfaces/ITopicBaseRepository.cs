eb765a98e0aeb15d2d947ccf05d0873a201df271
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-02-16 10:34:06 +0500

    DATA: TopicBaseRepository class and interface created

---
+++ LMS.Data/Interfaces/ITopicBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿using LMS.Domain;
+using System.Collections.Generic;
+
+namespace LMS.Data.Interfaces
+{
+    public interface ITopicBaseRepository : IEntityBaseRepository<Topic>
+    {
+        Task<IEnumerable<Topic>> GetAllTopicsAsync();
+        Task<Topic> GetTopicByIdAsync(int id);
+        Task<Topic> UpdateTopicAsync(int id, Topic topic);
+    }
+}
---
+++ LMS.Data/TopicBaseRepository.cs
@@ -0,0 +1,31 @@
+﻿using LMS.Data.Interfaces;
+using LMS.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace LMS.Data
+{
+    public class TopicBaseRepository : EntityBaseRepository<Topic>, ITopicBaseRepository
+    {
+        private readonly AppDbContext _context;
+        public TopicBaseRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Topic>> GetAllTopicsAsync() => await _context.Topics.Include(x => x.Course).ToListAsync();
+        public async Task<Topic> GetTopicByIdAsync(int id) => await _context.Topics.Include(x => x.Course).FirstOrDefaultAsync(x => x.Id == id);
+
+        public async Task<Topic> UpdateTopicAsync(int id, Topic topic)
+        {
+            var updateTopic = await _context.Topics.FirstOrDefaultAsync(x => x.Id == id);
+            if(updateTopic != null)
+            {
+                updateTopic.Name = topic.Name;
+                updateTopic.Date = topic.Date;
+                updateTopic.CourseId = topic.CourseId;
+            }
+            return updateTopic;
+        }
+    }
+}
