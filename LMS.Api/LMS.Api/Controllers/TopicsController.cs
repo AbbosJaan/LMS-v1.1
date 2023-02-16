66c9db547b251dd86e0ad69cdca678596566687e
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-02-16 11:01:39 +0500

    API: TopicsController successfully created

---
+++ LMS.Api/LMS.Api/Controllers/TopicsController.cs
@@ -0,0 +1,58 @@
+﻿using LMS.Service.Interfaces;
+using LMS.ViewModel.CreationViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TopicsController : ControllerBase
+    {
+        private readonly ITopicService _topicService;
+        public TopicsController(ITopicService topicService)
+        {
+            _topicService = topicService;
+        }
+
+        // GET: api/<TopicsController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _topicService.GetAllAsync());
+        }
+
+        // GET api/<TopicsController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = _topicService.GetByIdAsync(id);
+            if(result == null)
+                return NotFound("Topic is not found!");
+            return Ok(result);
+        }
+
+        // POST api/<TopicsController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] TopicCreationViewModel model)
+        {
+            var createTopic = await _topicService.CreateAsync(model);
+            if (createTopic == null)
+                return BadRequest("Topic creation failed!");
+            return Ok(createTopic);
+        }
+
+        // PUT api/<TopicsController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] TopicCreationViewModel model)
+        {
+            var updatedTopic = await _topicService.UpdateAsync(id, model);
+            if (updatedTopic == null)
+                return BadRequest("Topic updates failed");
+            return Ok(updatedTopic);
+        }
+
+        // DELETE api/<TopicsController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id) => Ok(await _topicService.DeleteAsync(id));
+    }
+}
