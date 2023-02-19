84b804174baa5ed43fe8f6acc1b757ffa07f2d15
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-02-19 20:17:48 +0500

    UI: Course creation is added

--- LMS.UI/Controllers/CoursesController.cs
+++ LMS.UI/Controllers/CoursesController.cs
@@ -1,4 +1,5 @@
 ﻿using LMS.Service.Interfaces;
+using LMS.ViewModel.CreationViewModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LMS.UI.Controllers
@@ -14,6 +15,22 @@ namespace LMS.UI.Controllers
         {
             var courses = await _courseService.GetAllAsync();
             return View(courses);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Name,StartDate,EndDate")] CourseCreationViewModel course)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(course);
+            }
+            await _courseService.CreateAsync(course);
+            return Redirect(nameof(Index));
         }
     }
 }
