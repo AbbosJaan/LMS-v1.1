2b5185021bd25311ca88c6dd2fdaeb6bf167840a
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-02-20 21:17:58 +0500

    UI: Course delete confirm method created

--- LMS.UI/Controllers/CoursesController.cs
+++ LMS.UI/Controllers/CoursesController.cs
@@ -11,6 +11,7 @@ namespace LMS.UI.Controllers
         {
             _courseService = courseService;
         }
+
         public async Task<IActionResult> Index()
         {
             var courses = await _courseService.GetAllAsync();
@@ -44,7 +45,23 @@ namespace LMS.UI.Controllers
         public async Task<IActionResult> Edit(int id, [Bind("Name,StartDate,EndDate")] CourseCreationViewModel course)
         {
             if(!ModelState.IsValid) return View(course);
+            
             await _courseService.UpdateAsync(id, course);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var course = await _courseService.GetByIdAsync(id);
+            if (course == null) return View("NotFound");
+            return View(course);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirm(int id)
+        {
+            var courseDetails = await _courseService.GetByIdAsync(id);
+            if( courseDetails == null) return View("NotFound");
+            await _courseService.DeleteAsync(id);
             return RedirectToAction(nameof(Index));
         }
     }
